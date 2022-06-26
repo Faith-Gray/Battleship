4c5769ea1d4db3730afa42276a5021d40f95631c
Faith <fhgray96@gmail.com>
2022-06-25 22:27:55 -0500

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    "entry": "./src/index.js"
+}
+
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ]
+};
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};

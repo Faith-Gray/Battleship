cf0f07d623373b624eed6efb0dabea2278e43e5e
Faith <fhgray96@gmail.com>
2022-06-27 19:03:03 -0500

    updated ship

--- src/objects/ship.js
+++ src/objects/ship.js
@@ -1,10 +1,13 @@
-const personFactory = (name, age) => {
+const ship = (length, isSunk, hit) => {
     const sayHello = () => console.log('hello!');
     return { name, age, sayHello };
 };
   
-const jeff = personFactory('jeff', 27);
+const battleShip = ship(4, false, 2);
   
 console.log(jeff.name); // 'jeff'
   
 jeff.sayHello(); // calls the function and logs 'hello
+
+
+//what should hit be?  a function? an array of hit spots?
